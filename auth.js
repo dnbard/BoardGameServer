9318d5cfb1a005a55adb64f39920afcae9e6b6b4
Alex Bardanov <dnbard@gmail.com>
2014-02-02 19:52:10 +0200

    login page

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 var config = require('./config.js');
 
 exports.restrict = function(req, res, next){
-    if (config.options.redirectWithoutAuth && (!res.locals.auth || !res.locals.user)){
+    if (config.options.redirectUri && (!res.locals.auth || !res.locals.user)){
         res.redirect(config.options.redirectUri);
     }
     next();
--- public/javascripts/controllers/login.js
+++ public/javascripts/controllers/login.js
@@ -4,7 +4,8 @@ $(document).ready(function(){
     var btnSwitchToRegistration = $('a.register'),
         btnSwitchToLogin = $('a.login'),
         panelLogin = $('div.login'),
-        panelRegistration = $('div.register');
+        panelRegistration = $('div.register'),
+        btnRegister = $('a.btnRegister');
 
     btnSwitchToRegistration.click(function(){
         panelLogin.fadeOut(fadeDelay);
@@ -15,4 +16,39 @@ $(document).ready(function(){
         panelRegistration.fadeOut(fadeDelay);
         panelLogin.delay(fadeDelay).fadeIn();
     });
+
+    var invalidateInput = function(input){
+        input.val('');
+        input.focus();
+        input.addClass('form-error');
+    }
+
+    btnRegister.click(function(){
+        var inptLogin = $('.form-login');
+        inptLogin.removeClass('form-error');
+
+        var login = inptLogin.val();
+        if (login.length == 0 || !login.match('[a-zA-Z]+')){
+            invalidateInput(inptLogin);
+            return;
+        }
+
+        var inptPassword = $('.form-password'),
+            inptPasscheck = $('.form-passcheck');
+        inptPassword.removeClass('form-error');
+        inptPasscheck.removeClass('form-error');
+
+        var password = inptPassword.val(),
+            passcheck = inptPasscheck.val();
+
+        if (password.length == 0 || !password.match('[a-zA-Z0-9]+')){
+            invalidateInput(inptPassword);
+            return;
+        }
+
+        if (password != passcheck){
+            invalidateInput(inptPasscheck);
+            return;
+        }
+    })
 });
